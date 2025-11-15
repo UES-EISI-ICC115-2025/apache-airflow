82b06b74f2f8218f4a047ac945e8b87c9d750890
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2025-11-15 16:06:42 -0600

    Refactor: Instalación inicial

--- create-db-script.sql
+++ create-db-script.sql
@@ -1,5 +1,35 @@
-CREATE DATABASE airflow_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'airflow_user'@'%' IDENTIFIED BY 'icc115';
-GRANT ALL PRIVILEGES ON airflow_db.* TO 'airflow_user'@'%';
-FLUSH PRIVILEGES;
-EXIT;
+-- sudo -u postgres psql
+--- Apache Airflow
+-- Create the database
+CREATE DATABASE airflow_db ENCODING 'UTF8';
+
+-- Create the user with a password
+CREATE USER airflow_user WITH PASSWORD 'icc115';
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON DATABASE airflow_db TO airflow_user;
+
+-- The following commands are for demonstration purposes to show how to connect to the new DB
+-- \c airflow_db;
+
+-- You may also need to grant privileges on future tables and sequences in the database
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO airflow_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO airflow_user;
+
+
+--- RASA
+CREATE DATABASE rasa_db ENCODING 'UTF8';
+
+-- Create the user with a password
+CREATE USER rasa_user WITH PASSWORD 'icc115';
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON DATABASE rasa_db TO rasa_user;
+
+-- The following commands are for demonstration purposes to show how to connect to the new DB
+-- \c rasa_db;
+
+-- You may also need to grant privileges on future tables and sequences in the database
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO rasa_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO rasa_user;
+-- \q
--- init.sh
+++ init.sh
@@ -1,21 +1,23 @@
 sudo apt update
 sudo apt install -y python3-pip python3-venv libpq-dev build-essential
-sudo apt install -y default-mysql-client default-libmysqlclient-dev pkg-config
-python3 -m venv ~/airflow-venv
-source ~/airflow-venv/bin/activate
+# sudo apt install -y default-mysql-client default-libmysqlclient-dev pkg-config
+python3 -m venv airflow-venv
+source airflow-venv/bin/activate
 
-sudo chown -R $(whoami):$(whoami) /srv/airflow-data
-sudo chmod -R 755 /srv/airflow-data
-
-export AIRFLOW_HOME=/srv/airflow-data
-export AIRFLOW__SQL_ALCHEMY_CONN='mysql+mysqldb://airflow_user:icc115@mysql-server:3306/airflow_db'
+sudo chown -R $(whoami):$(whoami) ~/apache-airflow/airflow-data
+sudo chmod -R 755 ~/apache-airflow/airflow-data
+sudo chmod u+x create-db-script.sql
+export AIRFLOW_HOME=/home/icc115/apache-airflow/airflow-data
+export AIRFLOW__SQL_ALCHEMY_CONN='postgresql+psycopg2://airflow_user:icc115@localhost:5432/airflow_db'
 export AIRFLOW_VERSION=3.0.3
+
+# sudo -u postgres psql
 
 # Extract the version of Python you have installed. If you're currently using a Python version that is not supported by Airflow, you may want to set this manually.
 # See above for supported versions.
-export PYTHON_VERSION="$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
+export PYTHON_VERSION="$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
 
 export CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 # For example this would install 3.0.0 with python 3.10: https://raw.githubusercontent.com/apache/airflow/constraints-3.1.0/constraints-3.10.txt
 
-pip install "apache-airflow[mysql]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install "apache-airflow[postgres]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
